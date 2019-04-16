bd3c4ac1fb6176ea078afff11d3d1098ae66a7ee
Praveen-Q <praveenguda.kumar@gmail.com>
2019-04-16 23:13:08 +0200

    Add basic validation

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Rentals from './components/rentals';
 import Customers from './components/customers';
 import NavBar from '../src/components/commons/navbar';
 import PageNotFound from '../src/components/commons/page-not-found';
+import LoginForm from './components/loginForm';
+
 import './App.css';
 
 class App extends Component {
@@ -16,15 +18,16 @@ class App extends Component {
 
         <NavBar />
         <main role="main" className="container">
-        <Switch>
-          <Route path="/movies/:id" component={MovieDetails} />
-          <Route path="/movies" component={Movies} />
-          <Route path="/customers" component={Customers} />
-          <Route path="/rentals" component={Rentals} />
-          <Route path="/page-not-found" component ={PageNotFound}/>
-          <Redirect from="/" exact to="/movies"/>
-          <Redirect to="/page-not-found"/>
-        </Switch>
+          <Switch>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies/:id" component={MovieDetails} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/page-not-found" component={PageNotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/page-not-found" />
+          </Switch>
         </main>
       </React.Fragment>
     );
--- src/components/commons/navbar.jsx
+++ src/components/commons/navbar.jsx
@@ -7,16 +7,18 @@ class NavBar extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <Link className="navbar-brand" to="/">Vidly</Link>
-               
+
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <div className="navbar-nav">
-                        
+
                         <NavLink className="nav-link nav-item" to="/movies">Movies</NavLink>
-                       
+
                         <NavLink className="nav-link nav-item" to="/customers">Customers</NavLink>
-                        
+
                         <NavLink className="nav-link nav-item" to="/rentals">Rentals</NavLink>
-                        
+
+                        <NavLink className="nav-link nav-item" to="/login">Login</NavLink>
+
                     </div>
                 </div>
             </nav>
