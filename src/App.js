02d72840b1ae50af3ffceb0c9026da0a0abf7f18
Praveen-Q <praveenguda.kumar@gmail.com>
2019-04-16 19:13:03 +0200

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movies from './components/movies';
+import Rentals from './components/rentals';
+import Customers from './components/customers';
+import NavBar from '../src/components/commons/navbar';
+import PageNotFound from '../src/components/commons/page-not-found';
+import { Route, Redirect, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <main role="main" className="container">
-      <Movies />
-      </main> 
+      <div>
+
+        <NavBar />
+        <main role="main" className="container">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/page-not-found" component ={PageNotFound}/>
+          <Route path="/" exact component={Movies}/>
+          <Redirect to="/page-not-found"/>
+        </Switch>
+        </main>
+      </div>
     );
   }
 }
---
+++ src/components/commons/navbar.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class NavBar extends Component {
+
+    render() {
+        return (
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <Link class="navbar-brand" to="/">Vidly</Link>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <Link class="nav-link" to="/movies">Movies <span class="sr-only">(current)</span></Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/customers">Customers</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/rentals">Rentals</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+export default NavBar;
